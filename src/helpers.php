319a6e3bb0e757b3902d224a98b0d09bde308097
dieter vanhove <dietervanhove@dieters-MacBook-Pro.local>
2022-08-11 17:25:22 +0200

    fix mulilang value

--- src/helpers.php
+++ src/helpers.php
@@ -6,16 +6,27 @@ if (! function_exists('esign_inline')) {
         if (session()->get('is_editing')) {
             return \Esign\InlineEdit\InlineEdit::string($term, $replaces);
         }
-
+        
         $service = new \Esign\InlineEdit\Services\TranslationService();
-
+        
         $result = $service->findForTerm($term);
 
-        if (empty($result->value)) {
-            return show_term_for_dev_env($term);
+        if (config('inline-edit.is_multilang')) {
+            $lang = app()->getLocale();
+
+            if (empty($result->{"value_$lang"})) {
+                return show_term_for_dev_env($term);
+            }
+    
+            $string = $result->{"value_$lang"};
+        } else {
+            if (empty($result->value)) {
+                return show_term_for_dev_env($term);
+            }
+    
+            $string = $result->value;
         }
 
-        $string = $result->value;
 
         foreach ($replaces as $search => $replace) {
             $string = str_replace($search, $replace, $string);
