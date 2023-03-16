ff6a2b80237aada443b46e181cb253e6f70f0ca0
Seppe Clottemans <seppe@esign.eu>
2023-03-16 13:39:34 +0100

    Update: test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,14 +1,13 @@
 <?php
 
-namespace :vendor_namespace\Tests;
-
-use :vendor_namespace\:studly_package_nameServiceProvider;
+namespace Esign\InlineEdit\Tests;
+use Esign\InlineEdit\InlineEditServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     protected function getPackageProviders($app): array
     {
-        return [:studly_package_nameServiceProvider::class];
+        return [InlineEditServiceProvider::class];
     }
-} 
+}
